40a8684a9f57310afec464db3c503ab09e4f0219
Nicolas <ncorantehuenchuman@gmail.com>
2024-10-25 13:56:20 -0300

    Modificacion Navbar, actualizacion vista HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
-<script setup>
+<script>
+import NavBar from './components/NavBar.vue';
+export default{
+    components:{
+        NavBar
+    }
+}
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-md navbar-dark">
-        <div class="container-fluid">
-            <span class="navbar-brand" href="#">Nicolas C.</span>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
-                <div class="navbar-nav m-auto">
-                    <router-link class="link-light link-underline-opacity-0 link-opacity-75-hover px-3 " to="/">
-                        Home
-                    </router-link>
-                    <router-link class="link-light link-underline-opacity-0 link-opacity-75-hover px-3"
-                        to="/AcercaDeMi">Acerca de Mi</router-link>
-                    <router-link class="link-light link-underline-opacity-0 link-opacity-75-hover px-3"
-                        to="/MisProyectos">Mis Proyectos</router-link>
-                    <router-link class="link-light link-underline-opacity-0 link-opacity-75-hover px-3"
-                        to="/Contacto">Contacto</router-link>
-                </div>
-            </div>
-
-        </div>
-    </nav>
+    <NavBar/>
 
     <router-view />
 </template>
 
 <style scoped>
-nav {
-    background-color: #121116;
-}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,18 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="container text-center">
+  <div class="row">
+    <div class="col-12 col-md-6 d-flex flex-column justify-content-center">
+      <h1 class="mt-4">Hola Soy Nicolas</h1>
+      <p class="align-middle">Bienvenido a mi portafolio, mi nombre es Nicolas y soy Desarrollador FrontEnd enfocado a diseñar paginas web
+        atractivas y funcionales
+      </p>
+      <a href="../public/CV.pdf" class="btn btn-primary mx-auto" target="_blank">CV</a>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="../assets/developer-working-illustration-png.webp" alt="">
+    </div>
   </div>
+</div>
 </template>
